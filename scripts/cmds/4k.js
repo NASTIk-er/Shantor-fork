828fd36b304633eef3dfffa0a39a06cc22808122
SHANTO-AHMED-GOATBOT <cuteshanto455@gmail.com>
2025-06-27 11:32:34 +0600

    Update 4k.js

--- scripts/cmds/4k.js
+++ scripts/cmds/4k.js
@@ -1,52 +1,43 @@
-const a = require('axios');
-const tinyurl = require('tinyurl');
+const axios = require("axios");
 
 module.exports = {
   config: {
     name: "4k",
-    aliases: ["6k", "upscale"],
-    version: "1.0",
-    author: "MR.AYAN",
-    countDown: 15,
+    aliases: ["hd"],
+    version: "1.1",
     role: 0,
-    longDescription: "Upscale your image.",
+    author: "Team Calyx",
+    countDown: 5,
+    longDescription: "Upscale images to 4K resolution.",
     category: "image",
     guide: {
-      en: "{pn} reply to an image"
+      en: "${pn} reply to an image to upscale it to 4K resolution."
     }
   },
+  onStart: async function ({ message, event }) {
+    if (!event.messageReply || !event.messageReply.attachments || !event.messageReply.attachments[0]) {
+      return message.reply("Please reply to an image to upscale it.");
+    }
+    const imgurl = encodeURIComponent(event.messageReply.attachments[0].url);
+    const noobs = 'xyz';
+    const upscaleUrl = `https://smfahim.onrender.com/4k?url=${imgurl}`;
+    
+    message.reply("🔄| Processing... Please wait a moment.", async (err, info) => {
+      try {
+        const { data: { image } } = await axios.get(upscaleUrl);
+        const attachment = await global.utils.getStreamFromURL(image, "upscaled-image.png");
 
-  onStart: async function ({ message, args, event, api }) {
-    let imageUrl;
+        message.reply({
+          body: "✅| Here is your 4K upscaled image:",
+          attachment: attachment
+        });
+        let processingMsgID = info.messageID;
+        message.unsend(processingMsgID);
 
-    if (event.type === "message_reply") {
-      const replyAttachment = event.messageReply.attachments[0];
-
-      if (["photo", "sticker"].includes(replyAttachment?.type)) {
-        imageUrl = replyAttachment.url;
-      } else {
-        return api.sendMessage(
-          { body: "❌ | Reply must be an image." },
-          event.threadID
-        );
+      } catch (error) {
+        console.error(error);
+        message.reply("❌| There was an error upscaling your image.");
       }
-    } else if (args[0]?.match(/(https?:\/\/.*\.(?:png|jpg|jpeg))/g)) {
-      imageUrl = args[0];
-    } else {
-      return api.sendMessage({ body: "❌ | Reply to an image." }, event.threadID);
-    }
-
-    try {
-      const url = await tinyurl.shorten(imageUrl);
-      const k = await a.get(`https://www.api.vyturex.com/upscale?imageUrl=${url}`);
-
-      message.reply("✅ | Please wait...");
-
-      const resultUrl = k.data.resultUrl;
-
-      message.reply({ body: "✅ | Image Upscaled.", attachment: await global.utils.getStreamFromURL(resultUrl) });
-    } catch (error) {
-      message.reply("❌ | Error: " + error.message);
-    }
+    });
   }
-}; 
+};
